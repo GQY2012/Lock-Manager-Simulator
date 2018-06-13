d39ebc7d31c9b605d95744907b01b41f210adf49
GQY2012 <958507333@qq.com>
2018-06-13 12:26:40 +0800

    2.0

--- Lock Manager Simulator/Lock Manager Simulator.cpp
+++ Lock Manager Simulator/Lock Manager Simulator.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
 #include <queue>
+#include <cstdio>
 #include <Windows.h>
 
 using namespace std;
 
-#define N 26
-#define M 256
+#define MAX_ObjectNum 26
+#define MAX_TransNum 256
 
 enum lock {
 	slock,
@@ -14,103 +15,243 @@ enum lock {
 	xlock,
 };
 
-int (*lock)[N] = new int[M][N]; //lock[transactionID][(int) object] == 'S' transactionID在object有S锁
-queue <int> *qs[N];
-queue <int> *qx[N];
+
+typedef struct object {
+	char objectID;
+	char lock[MAX_TransNum];
+	queue<int> q;
+};
+
+int trans[MAX_TransNum];
+object ob[MAX_ObjectNum];
 
 void startTransID(int transactionID) {
+	trans[transactionID] = 1;
 	cout << "Transaction started" << endl;
 }
 
-bool isSlock(char object) {
-	for (int i = 0;i < M;i++) {
-		if (lock[i][(int) object] == slock) {
-			return true;
-			break;
-		}
-	}
-	return false;
+bool isTransStart(int transactionID) {
+	return (trans[transactionID] == 1)? true:false;
 }
 
-bool isXlock(char object) {
-	for (int i = 0;i < M;i++) {
-		if (lock[i][(int) object] == xlock) {
-			return true;
-			break;
+int *isSlock(int objectID,bool& flag) {
+	int *p = new int[MAX_TransNum];
+	for (int i = 0;i < MAX_TransNum;i++)
+		p[i] = -1;
+	int *h = p;
+	for (int i = 0;i < MAX_TransNum;i++) {
+		if (ob[objectID].lock[i] == slock) {
+			flag = true;
+			*p = i;
+			p++;
 		}
 	}
-	return false;
+	return h;
 }
 
-void slockTransObject(int transactionID, char object) {
-	if (isXlock(object)) {
-		if (qs[(int)object] == NULL) {
-			qs[(int)object] = new queue<int>;
+int *isXlock(int objectID,bool& flag) {
+	int *p = new int[MAX_TransNum];
+	for (int i = 0;i < MAX_TransNum;i++)
+		p[i] = -1;
+	int *h = p;
+	for (int i = 0;i < MAX_TransNum;i++) {
+		if (ob[objectID].lock[i] == xlock) {
+			flag = true;
+			*p = i;
+			p++;
 		}
-		if (!(*qs[(int)object]).empty()) {
-			cout << "Waiting for lock (X-lock held by: ";
-			int size = (*qs[(int)object]).size();
-			int *temp = new int[size / 2];
-			for (int i = 0;i < size;i+=2) {
-				cout << (*qs[(int)object]).front() << (*qs[(int)object]).front << " ";
-				temp[i/2] = (*qs[(int)object]).pop();
-			}
-			cout << ")" << endl;
-			for (int i = 0;i < size;i += 2) {
-				(*qs[(int)object]).push(temp[i / 2]);
-			}
-		}
-		(*qs[(int)object]).push(transactionID);
-		(*qs[(int)object]).push(slock);
+	}
+	return h;
+}
+
+
+bool slockTransObject(int transactionID, int objectID) {
+	if (!isTransStart(transactionID)) {
+		printf("transaction %d not started\n", transactionID);
+		return false;
+	}
+
+	bool sflag = false, xflag = false;
+	int *ps = isSlock(objectID,sflag);
+	int *px = isXlock(objectID,xflag);
+	if (!xflag) {
+		ob[objectID].lock[transactionID] = slock;
+		printf("SLock granted to <%d>\n", transactionID);
+		return true;
 	}
 	else {
-		lock[transactionID][(int)object] = slock;
-		cout << "Lock granted" << endl;
+		ob[objectID].q.push(transactionID);
+		ob[objectID].q.push(slock);
+		printf("Waiting for lock (X-lock held by: <%d>)\n", px[0]);
+		return false;
 	}
 }
 
-void xlockTransObject(int transactionID, char object) {
-	if (lock[transactionID][(int)object] == slock) {
-		lock[transactionID][(int)object] = xlock;
-		cout << "Upgrade to X-lock granted " << endl;
+bool xlockTransObject(int transactionID, int objectID) {
+	if (!isTransStart(transactionID)) {
+		printf("transaction %d not started\n", transactionID);
+		return false;
 	}
-	else if (isSlock(object)) {
-		if (qs[(int)object] == NULL) {
-			qs[(int)object] = new queue<int>;
+
+	bool sflag = false, xflag = false;
+	int *ps = isSlock(objectID, sflag);
+	int *px = isXlock(objectID, xflag);
+	if (!xflag && !sflag) {
+		ob[objectID].lock[transactionID] = xlock;
+		printf("XLock granted to <%d>\n", transactionID);
+		return true;
+	}
+	else if (sflag) {
+		if (ps[0] == transactionID && ps[1] == -1) {
+			ob[objectID].lock[transactionID] = xlock;
+			cout << "Upgrade to X-lock granted" << endl;
+			return true;
 		}
-		if (!(*qs[(int)object]).empty()) {
-			cout << "Waiting for lock (s-lock held by: ";
-			int size = (*qs[(int)object]).size();
-			int *temp = new int[size / 2];
-			for (int i = 0;i < size;i += 2) {
-				cout << (*qs[(int)object]).front() << (*qs[(int)object]).front << " ";
-				temp[i / 2] = (*qs[(int)object]).pop();
+		else {
+			ob[objectID].q.push(transactionID);
+			ob[objectID].q.push(xlock);
+			cout << "Waiting for lock (S-lock held by:";
+			for (int i = 0;i < MAX_TransNum;i++) {
+				if (ps[i] == -1)
+					break;
+				else {
+					cout << "<" << ps[i] << ">";
+				}
 			}
 			cout << ")" << endl;
-			for (int i = 0;i < size;i += 2) {
-				(*qs[(int)object]).push(temp[i / 2]);
+			return false;
+		}
+	}
+	else {
+		ob[objectID].q.push(transactionID);
+		ob[objectID].q.push(xlock);
+		printf("Waiting for lock (X-lock held by: <%d>)\n", px[0]);
+		return false;
+	}
+}
+
+void unlockTransObject(int transactionID, int objectID) {
+	if (!isTransStart(transactionID)) {
+		printf("transaction %d not started\n", transactionID);
+		return;
+	}
+
+	bool sflag = false, xflag = false;
+	int *ps = isSlock(objectID, sflag);
+	int *px = isXlock(objectID, xflag);
+
+	ob[objectID].lock[transactionID] = -1;
+	cout << "Lock released" << endl;
+	if (!ob[objectID].q.empty()) {
+		int transID = ob[objectID].q.front();
+		ob[objectID].q.pop();
+		int lock = ob[objectID].q.front();
+		ob[objectID].q.pop();
+		if (lock == slock) {
+			slockTransObject(transID, objectID);
+		}
+		else {
+			xlockTransObject(transID, objectID);
+		}
+		
+	}
+}
+
+void endTransID(int transactionID) {
+	if (!isTransStart(transactionID)) {
+		printf("transaction %d not started\n", transactionID);
+		return;
+	}
+
+	trans[transactionID] = 0;
+	printf("Transaction %d ended\n",transactionID);
+	for (int i = 0;i < MAX_ObjectNum;i++) {
+		int newqsize, qsize;
+		newqsize = qsize = ob[i].q.size();
+		int temp[MAX_TransNum];
+		for (int j = 0;j < qsize;j += 2) {
+			if (ob[i].q.front() == transactionID) {
+				ob[i].q.pop();
+				ob[i].q.pop();
+				newqsize -= 2;
+			}
+			else {
+				temp[j] = ob[i].q.front();
+				ob[i].q.pop();
+				temp[j + 1] = ob[i].q.front();
+				ob[i].q.pop();
 			}
 		}
-		(*qs[(int)object]).push(transactionID);
-		(*qs[(int)object]).push(xlock);
+		for (int j = 0;j < newqsize;j += 2) {
+			ob[i].q.push(temp[j]);
+			ob[i].q.push(temp[j+1]);
+		}
+		//end队列中的事务
+
+		
+		if (ob[i].lock[transactionID] != -1) {
+			if (ob[i].lock[transactionID] == slock) {
+				printf("Release S-lock on %c\n", i + 'A');
+			}
+			else if(ob[i].lock[transactionID] == xlock) {
+				printf("Release X-lock on %c\n", i + 'A');
+			}
+			ob[i].lock[transactionID] = -1;
+
+			if (!ob[i].q.empty()) {
+				int transID = ob[i].q.front();
+				ob[i].q.pop();
+				int lock = ob[i].q.front();
+				ob[i].q.pop();
+				if (lock == slock) {
+					slockTransObject(transID, i);
+				}
+				else {
+					xlockTransObject(transID, i);
+				}
+
+			}
+		}
 	}
 }
 
 int main() 
 {
-	for (int i = 0;i < M;i++) {
-		for (int j = 0;j < N;j++) {
-			lock[i][j] = -1;
+	for (int i = 0;i < MAX_ObjectNum;i++) {
+		for (int j = 0;j < MAX_TransNum;j++) {
+			ob[i].lock[j] = -1;
 		}
 	}
-
+	memset(trans, 0, MAX_TransNum);
 	string requestType;
 	int transactionID;
 	char object;
 
 	while (true) {
 		cin >> requestType >> transactionID >> object;
-		
+		int objectID = object - 'A';
+
+		if (requestType == "start") {
+			startTransID(transactionID);
+		}
+		else if (requestType == "slock") {
+			slockTransObject(transactionID, objectID);
+		}
+		else if (requestType == "xlock") {
+			xlockTransObject(transactionID, objectID);
+		}
+		else if (requestType == "unlock") {
+			unlockTransObject(transactionID, objectID);
+		}
+		else if (requestType == "end") {
+			endTransID(transactionID);
+		}
+		else if (requestType == "quit") {
+			break;
+		}
+		else {
+
+		}
 	}
 
 	system("pause");
